5d6cfb9a50f2752bfb16779b28fd010409e09035
Faouzii <moad.faouzi@gmail.com>
2021-04-22 00:43:03 +0000

    jobs contents

--- src/app/components/job/job.component.ts
+++ src/app/components/job/job.component.ts
@@ -33,23 +33,40 @@ export class JobComponent implements OnInit {
     ]},
 
     {title : "Full Stack Software Engineer", company : "SafeDemat", date : "Sep 2019 - Oct 2020", missions : [
-      {name : "mission 2 name", stacks : [
-        {name : "JAVA"},
-        {name: 'PrimeNg'},
-        {name: 'Angular Material'},
+      {name : "Developed RESTFULL Web Services and back-end APIs.", stacks : [
+        {name : "JAVA 8"},
+        {name: 'Spring boot'},
+        {name: 'MS SQL Server'},
+      ]},
+
+      {name : "Developed web applications from scratch.", stacks : [
+        {name : "JAVA 8"},
+        {name: 'Spring'},
+        {name: 'Jasper Reports'},
+        {name: 'MySQL'},
+        {name: 'Bootstrap'},
+        {name: 'jQuery'},
+      ]},
+      {name : "Maintained existing applications to correct bugs, improuve performance and add new features.", stacks : [
+        {name : "JAVA 8"},
+        {name: 'Spring'},
+        {name: 'MySQL'},
+        {name: 'Bootstrap'},
+        {name: 'jQuery'},
       ]}
     ]},
 
     {title : "Full Stack Software Engineer Intern", company : "SafeDemat", date : "Sep 2019 - Oct 2020", missions : [
-      {name : "mission 2 name", stacks : [
+      {name : "Developed Backend REST API ", stacks : [
         {name : "JAVA"},
-        {name: 'PrimeNg'},
-        {name: 'Angular Material'},
+      ]},
+      {name : "Developed Front-end and migrated an existing application from Struts 2 to Angular 2+ ", stacks : [
+        {name : "Angular"},
       ]}
     ]},
     
     {title : "Summer Intern", company : "SafeDemat", date : "Sep 2019 - Oct 2020", missions : [
-      {name : "mission 2 name", stacks : [
+      {name : "Designed, created and implemented solution for migrating JAVA web applications based on Struts2 to Spring boot.", stacks : [
         {name : "JAVA"},
         {name: 'PrimeNg'},
         {name: 'Angular Material'},
