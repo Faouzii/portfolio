54badfa52a2beb70c9af6c7b4450a9df27d256de
Faouzii <34894595+Faouzii@users.noreply.github.com>
2021-05-27 15:04:34 +0100

    UI Update

--- src/app/components/job/job.component.ts
+++ src/app/components/job/job.component.ts
@@ -18,15 +18,17 @@ export class JobComponent implements OnInit {
 
   jobs : Job [] = [
     {title : "Full Stack Software Engineer", company : "EURAFRIC Information - Bank of africa", date : "Nov 2020 - Current" , missions : [
-      {name : "Etudes et développement des RESTful Web Service et des back-end APIs (Secteur Bancaire) ", stacks : [
-        {name : "JAVA 8"},
-        {name: 'Spring (core, boot, MVC, Data)'},
+      {name : "Developed RESTFULL Web Services and back-end APIs for banking applications ", stacks : [
+        {name : "Java 8"},
+        {name: 'Spring boot'},
+        {name: 'Spring MVC'},
+        {name: 'Spring Data'},
         {name: 'Maven'},
         {name: 'Git'},
         {name: 'JWT'},
-        {name: 'SQL Server'}
+        {name: 'Oracle'}
       ]},
-      {name : "Conception et développement de la partie front-end", stacks : [
+      {name : "Designed and developed front-end applications", stacks : [
         
         {name: 'Angular 8'},
         {name: 'Javascript'},
@@ -38,65 +40,100 @@ export class JobComponent implements OnInit {
         {name : "Git"}
       ]},
      
-      {name : "Développement des batches JAVA pour traitement des données massive ", stacks : [
-        {name : "JAVA 8"},
+      {name : "Developed Java batches for massive data processing", stacks : [
+        {name : "Java 8"},
         {name : "Git"}
       ]}
     ]},
 
     {title : "Full Stack Software Engineer", company : "SafeDemat", date : "Sep 2019 - Oct 2020", missions : [
       {name : "Developed RESTFULL Web Services and back-end APIs.", stacks : [
-        {name : "JAVA 8"},
-        {name: 'Spring boot'},
+        {name : "Java 8"},
+        {name: 'Spring Boot'},
+        {name: 'Spring MVC'},
+        {name: 'Spring Data'},
+        {name: 'JPA'},
+        {name: 'SQL Server'},
         {name: 'Maven'},
         {name: 'JWT'},
-        {name: 'MS SQL Server'},
+        {name: 'Git'},
+        {name: 'JSON'},
       ]},
 
+
       {name : "Developed web applications from scratch.", stacks : [
-        {name : "JAVA 8"},
-        {name: 'Spring'},
+        {name : "Java 8"},
+        {name: 'Spring Boot'},
+        {name: 'Spring MVC'},
+        {name: 'Spring Data'},
+        {name: 'JPA'},
         {name: 'Jasper Reports'},
         {name: 'MySQL'},
+        {name: 'Maven'},
+        {name: 'Git'},
         {name: 'JSP'},
         {name: 'Bootstrap'},
         {name: 'jQuery'},
       ]},
       {name : "Maintained existing applications to correct bugs, improuve performance and add new features.", stacks : [
-        {name: "JAVA 8"},
+        {name: "Java 8"},
         {name: 'Spring'},
         {name: 'Struts 2'},
+        {name: 'Hibernate'},
+        {name: 'JPA'},
         {name: 'MySQL'},
-        {name: 'Bootstrap'},
+        {name: 'JSP'},
+        {name: 'Bootstrap 4'},
         {name: 'jQuery'},
+        {name: 'Javascript'},
+        {name: 'Git'},
       ]}
     ]},
 
+
     {title : "Full Stack Software Engineer Intern", company : "SafeDemat", date : "Sep 2019 - Oct 2020", missions : [
-      {name : "Developed Backend REST API ", stacks : [
-        {name : "JAVA"},
-      ]},
       {name : "Developed Front-end and migrated an existing application from Struts 2 to Angular 2+ ", stacks : [
-        {name : "Angular"},
+        {name : "Angular 4"},
+        {name : "Boostrap 4"},
+        {name : "SCSS"},
+        {name : "NPM"},
+        {name : "Git"},
       ]}
+      ,
+      {name : "Developed Backend REST API ", stacks : [
+        {name : "Java 8"},
+        {name : "Spring"},
+        {name : "JAX-RS"},
+        {name : "Jersey 2"},
+        {name : "Jackson"},
+        {name : "JSON"},
+        {name : "Hibernate"},
+        {name : "JPA"},
+        {name : "MySQL"},
+        {name : "JWT"},
+      ]},
+      
     ]},
     
     {title : "Summer Intern", company : "SafeDemat", date : "Sep 2019 - Oct 2020", missions : [
       {name : "Designed, created and implemented solution for migrating JAVA web applications based on Struts2 to Spring boot.", stacks : [
-        {name : "JAVA"},
-        {name: 'PrimeNg'},
-        {name: 'Angular Material'},
-      ]}
-    ]}
-  ];
+        {name : "Java 8"},
+        {name: 'Spring'},
+        {name: 'JAX-RS'},
+        {name: 'Jersey 2'},
+        {name: 'Struts 2'},
+        {name: 'JSON'},
+      ]},
 
-  vegetables: Stack[] = [
-    {name: 'JAVA 8'},
-    {name: 'Angular 8'},
-    {name: 'Javascript'},
-    {name: 'SCSS'},
-    {name: 'Angular Material'},
-    {name: 'PrimeNg'},
+      {name : "Designed, created and implemented solution for migrating JAVA web applications based on Struts2 to Spring boot.", stacks : [
+        {name : "Java 8"},
+        {name: 'Spring boot'},
+        {name: 'Inno Setup'},
+        {name: 'Pascal'}
+
+      ]}
+
+    ]}
   ];
 
   drop(event){
@@ -104,3 +141,38 @@ export class JobComponent implements OnInit {
   }
 }
 
+/*
+  des RESTful Web Services*
+  Oracle
+
+  massives*
+
+  SZ REST
+  Java 8, Spring (Boot, MVC, Data), JPA, SQL Server, Maven, Git, JWT, JSON
+
+  App signature
+  Java 8, Spring (Boot, MVC, Data), JPA, Jasper Report, MySQL, Maven, Git, JSP, Bootstrap, jQuery
+
+  maintain
+  Java 8, Spring, Struts 2, Hibernate, JPA, MySQL, JSP, Bootstrap 4, jQuery, JavaScript, Git
+
+  migration
+  Angular 4, Bootstrap 4, SCSS, NPM, Git
+
+  Java 8, Spring, JAX-RS, Jersey 2, Jackson, JSON, Hibernate, JPA, MySQL, JWT
+
+  stage app
+  Java 8, Spring boot, Innp Setup, Pascal
+
+
+  Ana w l'coloc est une application 100% Maroccain, 100% gratuite et sans pubs, qui permet de suivre et gérer les dépenses partagées entre les colocataires, les amis, la famille.. dans le but de faciliter la vie quotidienne.
+
+  Application de developement personelles base sur le principe Task - Reward, qui vous motive a attiendre vos objectives.
+  Apres avoir definir votre Reward, et pour chaque tache realise vous gagner des point pour unlocker.
+
+  Contribution Angular : ngx-timeago-pipe-fr
+  Une contribution open-source à la communauté Angular qui permet de formater les dates sous format timeago en français : Il y a 3 heures..
+  Le package est disponible sur NPM (npm install ngx-timeago-pipe-fr)
+
+
+  */
